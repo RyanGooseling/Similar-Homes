3fed60ca5dd566602086e4e9ccb059f8e2cf1b74
Nathaniel Schwartz <nathanielhschwartz@gmail.com>
2020-10-05 11:52:04 -0400

    additional style changes and deleted unused component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import HomesList from './HomesList.jsx';
-import Carousel from './Carousel.jsx';
-import axios from 'axios';
 
 const App = (props) => (
   <HomesList homes={props.homes} />
